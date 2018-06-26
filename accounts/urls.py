c4fb869e5b97d6982fea331a9ff910787639ecbc
Ashiri <ajaoiyanu@gmail.com>
2018-06-26 03:41:21 +0100

    Add send_message url

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/<admin_id>/agent/signup/', views.agent_signup, name='agent_signup'),
     path('accounts/admin/signup/', views.AdminSignUpView.as_view(), name='admin_signup'),
+    path('accounts/<admin_id>/send_message/', views.send_message, name='send_message')
 ]
