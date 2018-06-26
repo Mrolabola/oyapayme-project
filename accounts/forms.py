78f8f780fe1cde7ce9e06ce93cf1c51bba8948dd
Ashiri <ajaoiyanu@gmail.com>
2018-06-26 03:43:08 +0100

    Add create agent form

--- accounts/forms.py
+++ accounts/forms.py
@@ -27,3 +27,10 @@ class AgentSignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('phone_number', 'fullname', 'password1', 'password2')
+    # I could have override the save method of here, but that will cause a difficulty when trying to
+    # implement an agent signup view function that will receive an instance of the associated admin
+    # instance.
+
+
+class CreateAgentForm(forms.Form):
+    agent_phone_number = forms.CharField(max_length=12)
